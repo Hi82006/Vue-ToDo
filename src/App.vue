660164f9e071de0cb9177495489aa2f67ed2a371
Hi82006 <Hi82006@gmail.com>
2023-06-27 21:50:37 +0300

    commit

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,53 @@
 <template>
-  <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </nav>
-  <router-view/>
+  <div class="app">
+    <post-form/>
+    <post-list :posts="posts"/>
+    
+  </div>
 </template>
+<script>
+import PostForm from '@/components/PostForm';
+import PostList from '@/components/PostList';
+
+export default {
+  components: {
+    PostList, PostForm
+  },
+  data() {
+    return {
+      posts: [
+        { id: 1, title: "Javascript", body: "Описание поста" },
+        { id: 2, title: "Javascript", body: "Описание поста" },
+        { id: 3, title: "Javascript", body: "Описание поста" },
+      ],
+      title: '',
+      body: '',
+    };
+  },
+  methods: {
+    createPost() {
+      const newPost = {
+        id: Date.now(),
+        title: this.title,
+        body: this.body,
+      }
+      this.posts.push(newPost);
+      this.title = '';
+      this.body = '';
+    },
+  },
+  
+};
+</script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
+.app {
+  padding: 20px;
 }
 
-nav {
-  padding: 30px;
-}
-
-nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-nav a.router-link-exact-active {
-  color: #42b983;
-}
 </style>
--- src/main.js
+++ src/main.js
@@ -1,4 +1,4 @@
 import { createApp } from 'vue'
-import App from './App.vue'
+import App from './App'
 
-createApp(App).use(store).use(router).mount('#app')
+createApp(App).mount('#app')

de0ac7795bfca1c94b375f4bc4bd0ab1d22483d7
Hi82006 <Hi82006@gmail.com>
2023-06-28 12:11:12 +0300

    commit

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,11 @@
 <template>
   <div class="app">
-    <post-form/>
-    <post-list :posts="posts"/>
+    <post-form
+    @create="createPost"
+    />
+    <post-list 
+    :posts="posts"
+    />
     
   </div>
 </template>
@@ -20,20 +24,14 @@ export default {
         { id: 2, title: "Javascript", body: "Описание поста" },
         { id: 3, title: "Javascript", body: "Описание поста" },
       ],
-      title: '',
-      body: '',
     };
   },
   methods: {
-    createPost() {
-      const newPost = {
-        id: Date.now(),
-        title: this.title,
-        body: this.body,
-      }
-      this.posts.push(newPost);
-      this.title = '';
-      this.body = '';
+    createPost(post, second, third) {
+      console.log(post)
+      console.log(second)
+      console.log(third)
+      this.posts.push(post)
     },
   },
   
--- src/components/PostForm.vue
+++ src/components/PostForm.vue
@@ -1,38 +1,49 @@
 <template>
-    <form @submit.prevent>
-      <h4>Создание поста</h4>
-      <input 
-        v-bind:value="post.title"
-        @input="post.title = $event.target.value" 
-        class="input" 
-        type="text" 
-        placeholder="Название" 
-      />
-      <input 
-        v-bind:value="post.body"
-        @input="post.body = $event.target.value" 
-        class="input" 
-        type="text" 
-        placeholder="Описание" 
-      />
-      <button class="btn" 
-      
-      >
-      Создать</button>
-    </form>
+  <form @submit.prevent>
+    <h4>Создание поста</h4>
+    <input
+      v-model="post.title"
+      class="input"
+      type="text"
+      placeholder="Название"
+    />
+    <input
+      v-model="post.body"
+      class="input"
+      type="text"
+      placeholder="Описание"
+    />
+    <my-button class="btn" 
+    style="align-self: flex-end;margin-top: 15px;"
+    @click="createPost"
+    >Создать
+    </my-button>
+  </form>
 </template>
 
 <script>
+import MyButton from './UI/MyButton.vue';
 export default {
-    data() {
-        return {
-            post: {
-                title: '',
-                body: '',
-            }
-        }
-    }
-}
+    components: {MyButton},
+  data() {
+    return {
+      post: {
+        title: "",
+        body: "",
+      },
+    };
+  },
+  methods: {
+    createPost() {
+      this.post.id = Date.now()
+      this.$emit('create', this.post, 'проба','проба2')
+      this.post = {
+        title: "",
+        body: "",
+      }
+    },
+  },
+};
 </script>
 
 <style scoped>
@@ -46,12 +57,5 @@ form {
   display: flex;
   flex-direction: column;
 }
-.btn {
-  margin-top: 15px;
-  align-self: flex-end;
-  padding: 10px 15px;
-  background: none;
-  color: teal;
-  border: 1px solid teal;
-}
+
 </style>
